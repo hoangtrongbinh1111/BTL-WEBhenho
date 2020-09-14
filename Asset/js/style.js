ebab5550a08d617278468e9fdbae2395ccb77b8d
Hoàng Trọng Bình <62093518+hoangtrongbinh1111@users.noreply.github.com>
2020-09-15 00:04:44 +0700

    Commit 3 15/9
    Frontend ghép vào ASP.NET MVC

--- Asset/js/style.js
+++ Asset/js/style.js
@@ -1,3 +1,11 @@
-$(function() {
+$(document).ready(function() {
+    $("#bt_close").click(function() {
+        $("#wrap_chat_max").css('display', 'none');
+        $("#wrap_chat_min").css('display', 'block');
+    });
+    $("#wrap_chat_min").click(function() {
+        $("#wrap_chat_max").css('display', 'block');
+        $("#wrap_chat_min").css('display', 'none');
+    });
 
-});
+})
